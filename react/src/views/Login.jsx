d4b858e907e1e6ceba9fb0176c8d66cffeea1390
Tuan <huynhbaotuan01@gmail.com>
2023-03-30 15:00:37 +0700

    TP-7 <Add Login page and NotFound>

---
+++ react/src/views/Login.jsx
@@ -0,0 +1,85 @@
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useRef } from "react";
+
+
+
+const theme = createTheme();
+
+
+export default function Login() {
+
+    const emailRef = useRef();
+    const passwordRef = useRef();
+
+    const handleLogin = async (event) => {
+        event.preventDefault();
+        const payload = {
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+        };
+
+    };
+
+    return (
+       <div> <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Đăng nhập
+                    </Typography>
+                    <Box sx={{ mt: 1 }}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            label="Email"
+                            autoComplete="email"
+                            autoFocus
+                            inputRef={emailRef}
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            label="Password"
+                            type="password"
+                            autoComplete="current-password"
+                            inputRef={passwordRef}
+                        />
+                        <Button
+                            disableElevation
+                            onClick={handleLogin}
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2, borderRadius: 10 }}
+                            size="large"
+                        >
+                            Đăng nhập
+                        </Button>
+                    </Box>
+                </Box>
+            </Container>
+        </ThemeProvider>
+        </div>
+    );
+}
