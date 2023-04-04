d14f8b124a28c91ec649170ab3a27cd6726e6c76
Tuan <huynhbaotuan01@gmail.com>
2023-04-04 12:58:15 +0700

    Add table heroes

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,11 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import NotFound from "./views/NotFound";
 import Admin from "./views/Admin";
+import TableHero from "./components/TableHero";
 
 const router = createBrowserRouter([
     {
         path: "/admin",
         element: <Admin />,
+        children: [           
+            {
+                index: "/admin",
+                element: <TableHero />,
+            },
+        ],
     },
 
     {
